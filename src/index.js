ff1addb2131031dae9db325fcec08cfbc68d9561
iMac <imac@iMac-khxng-iMac.local>
2019-02-22 11:31:29 +0700

    add index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 
+const App = () => {
+  return <div> Hi there !</div>;
+};
+
 // Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
